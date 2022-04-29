0260e1b9d15c748676efcc5b2cfa841cdbb56dbe
Anderson Correia <acorreia610@gmail.com>
2022-04-29 13:32:37 -0300

    Update and Delete OK

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,12 +18,13 @@ use DateTimeZone;
  */
 class CourseController extends AbstractController
 {
+
     /**
      * @Route("/", name="index", methods={"GET"})
      */
-    public function index()
+    public function index(EntityManagerInterface $entityManager) : Response
     {
-        $courses = $this->getDoctrine()->getRepository(Course::class)->findAll();
+        $courses = $entityManager->getRepository(Course::class)->findAll();
 
         return $this->json([
             'data' => $courses
@@ -32,9 +34,9 @@ class CourseController extends AbstractController
     /**
      * @Route("/{courseId}", name="show", methods={"GET"})
      */
-    public function show(int $courseId)
+    public function show(int $courseId, EntityManagerInterface $entityManager) : Response
     {
-        $course = $this->getDoctrine()->getRepository(Course::class)->find($courseId);
+        $course = $entityManager->getRepository(Course::class)->find($courseId);
 
         return $this->json([
             'data' => $course
@@ -44,7 +46,7 @@ class CourseController extends AbstractController
     /**
      * @Route("/", name="create", methods={"POST"})
      */   
-    public function create(Request $request)
+    public function create(Request $request, EntityManagerInterface $entityManager) : Response
     {
         $data = $request->request->all();
 
@@ -53,9 +55,8 @@ class CourseController extends AbstractController
         $course->setDescription($data['description']);
         $course->setSlug($data['slug']);
 
-        $doctrine = $this->getDoctrine()->getManager();
-        $doctrine->persist($course);
-        $doctrine->flush();
+        $entityManager->persist($course);
+        $entityManager->flush();
 
         return $this->json([
             'data' => 'Curso criado com sucesso!'
@@ -66,17 +67,37 @@ class CourseController extends AbstractController
     /**
      * @Route("/{courseId}", name="update", methods={"PUT", "PATCH"})
      */
-    public function update(int $courseId)
+    public function update(int $courseId, Request $request, EntityManagerInterface $entityManager) : Response
     {
+        $data = $request->request->all();
 
+        $course = $entityManager->getRepository(Course::class)->find($courseId);
+        if (isset($data['name'])) $course->setName($data['name']);
+        if (isset($data['description'])) $course->setDescription($data['description']);
+        if (isset($data['slug'])) $course->setSlug($data['slug']);
+
+        $entityManager->persist($course);
+        $entityManager->flush();
+
+        return $this->json([
+            'data' => 'Curso atualizado com sucesso!'
+        ]);
     }
 
     /**
      * @Route("/{courseId}", name="delete", methods={"DELETE"})
      */
-    public function delete(int $courseId)
+    public function delete(int $courseId, EntityManagerInterface $entityManager) : Response
     {
+        $course = $entityManager->getRepository(Course::class)->find($courseId);
 
+        $entityManager->remove($course);
+        $entityManager->flush();
+
+
+        return $this->json([
+            'data' => 'Curso removido com sucesso!'
+        ]);
     }
 
 }
